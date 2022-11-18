cd71d991e84bcf1b2cfad3ced02da4e6ef0fd36f
Daniel Miguel Baltes Amado <daniel@phunkei.de>
2022-11-18 10:19:26 +0100

    add to typeaddon

--- src/Phunkei/PhunkTheme/TypeAddons/TypeAddon.php
+++ src/Phunkei/PhunkTheme/TypeAddons/TypeAddon.php
@@ -4,6 +4,9 @@ namespace Phunkei\PhunkTheme\TypeAddons;
 class TypeAddon {
 	public function __construct() {
 		$this->registerType();
+		if( is_callable(self::class, 'registerMeta') ) {
+			call_user_func([$this, 'registerMeta']);
+		}
 	}
 
 	function registerType() {}
